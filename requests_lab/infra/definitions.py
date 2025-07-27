500810e93fafe5cb28ec81da6f54cfa97f68c467
Marilzon <maxmaril@hotmail.com>
2025-07-27 00:54:38 -0300

    update: get_starships

---
+++ requests_lab/infra/definitions.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://swapi.py4e.com/api"
+PLANETS = "planets"
+POEPLE = "people"
+STARSHIPS = "starships"
--- tests/requests_lab/test_inc.py
+++
@@ -1,4 +0,0 @@
-from requests_lab import inc
-
-def test_inc():
-  assert inc(3) == 4
